0d9d858ddc0a6a03bbfd545d533deafa58320a60
haievyiivan <haievyiivan@gmail.com>
2022-11-09 11:14:46 +0200

    Add project files.

---
+++ Factorial/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace App
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            // Enter x
+            Console.Write("x = ");
+            int x = int.Parse(Console.ReadLine()); 
+
+            ulong answer = 1; // Answer variable
+            // After 21-23 variable will be full
+
+            // Loop
+            for (uint i = 1; i <= x; i++)
+            {
+                answer *= i;
+            }
+
+            // Write answer to the console
+            Console.WriteLine($"{x}! = {answer}");
+
+            // ReadKey
+            Console.ReadKey();
+        }
+    }
+}
